1d54f8e63fa9f47f7cf4fac0383d3943a6e560d6
Héctor C. Alzate <halzate93@gmail.com>
2017-01-20 05:32:17 -0500

    Rigidbody controller

---
+++ Assets/Scripts/RigidbodyAgent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (RigidbodyController))]
+public class RigidbodyAgent : MonoBehaviour 
+{
+	private RigidbodyController rigidbodyController;
+
+	private void Awake ()
+	{
+		rigidbodyController = GetComponent<RigidbodyController> ();
+	}
+	
+	private void Update () {
+		// Check for input
+		if (Input.GetButtonDown ("Fire1"))
+			SetDestination ();
+	}
+
+	private void SetDestination()
+	{
+		// Construct a ray from the current mouse coordinates and check for ground
+		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit = new RaycastHit ();
+		if (Physics.Raycast (ray, out hit))
+		{
+			// Set agent destination to start pathfinding
+			rigidbodyController.SetDestination(hit.point);
+		}
+	}
+}
---
+++ Assets/Scripts/RigidbodyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (Animator))]
+public class RigidbodyController : MonoBehaviour 
+{
+	private NavMeshAgent agent;
+	private new Rigidbody rigidbody;
+	private Animator animator;
+
+	private void Awake ()
+	{
+		// Get references to components, shouldn't be null because of RequireComponent attribute
+		agent = GetComponent<NavMeshAgent> ();
+		rigidbody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	private void Update () 
+	{
+		// The path is recalculated every frame, and the movement can be more complex: dash, climb, etc.
+		rigidbody.velocity = agent.desiredVelocity;
+		SyncAnimation ();
+	}
+
+	public void SetDestination(Vector3 destination)
+	{
+		// Set the agents new destination
+		agent.destination = destination;
+	}
+
+	private void SyncAnimation ()
+	{
+		// Set speed parameter in animator
+		bool isMoving = CheckIsMoving ();
+		float speed = isMoving? 1f : 0f;
+		animator.SetFloat ("Speed", speed);
+	}
+
+	private bool CheckIsMoving ()
+	{
+		// Check if agent reached last node and the distance inside the node is close enough to the target
+		return agent.pathPending || agent.remainingDistance > agent.stoppingDistance;
+	}
+}
